d267ef8588a34e823ab06ea797f84a743202e657
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2023-05-22 00:26:42 +0300

    seed system updated

--- src/ChainTest.java
+++ src/ChainTest.java
@@ -1,11 +1,29 @@
+import java.util.Random;
 import java.util.Scanner;
 public class ChainTest {
     public static void main(String[] args) throws Exception {
+    	// Going to take to Chain.java class
         Scanner scn = new Scanner(System.in);
         System.out.print("Enter Name: ");
         String uname = scn.nextLine();
-        System.out.print("Enter seed: ");
-        Integer seed = Integer.valueOf(scn.nextLine());
+        System.out.print("Enter seed (If you leave it blank, a random seed is generated): ");
+        String seedInput = scn.nextLine();
+        
+        // Going to write to enigma console
+        int seed;
+        if (seedInput.isEmpty()) {
+            seed = new Random().nextInt(9000) + 1000; // 1000 ile 9999 arasında rastgele bir sayı oluştur
+            System.out.println("Oluşturulan rastgele tohum: " + seed);
+        } else {
+            try {
+                seed = Integer.parseInt(seedInput);
+            } catch (NumberFormatException e) {
+            	
+            	System.out.println("Hatalı tohum değeri girildi : " + seedInput);
+            	seed = new Random().nextInt(9000) + 1000; // 1000 ile 9999 arasında rastgele bir sayı oluştur
+            	System.out.println("Oluşturulan rastgele tohum: " + seed);
+            }
+        }
         scn.close();
         Chain chain = new Chain(seed,uname);
     }
